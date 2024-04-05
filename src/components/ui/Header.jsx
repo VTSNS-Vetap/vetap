8de10e74ba934e8cc8b0974df4faf67f9c6f2b48
boskoivez <boskovimejl@gmail.com>
2024-04-05 21:02:57 +0200

    AG-2-Layout Kreiranje Header-a

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,12 +1,53 @@
 
-const Header
- = () => {
-  return (
-    <>
-      Header
-    </>
-  )
-};
+import React, { useState } from 'react';
+import './Header.css';
 
-export default Header
-;
+const Header = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [showSettings, setShowSettings] = useState(false); 
+
+  const handleLoginClick = () => {
+   
+    setLoggedIn(true);
+  };
+
+  const handleLogoutClick = () => {
+    
+    setLoggedIn(false);
+  };
+
+  const handleSettingsClick = () => {
+    
+    setShowSettings(!showSettings);
+  };
+
+  return (
+    <header className="header">
+      <div className="left">
+        <img src={`${process.env.PUBLIC_URL}/favicon-32x32.png`} alt="Logo" className="logo" />
+        <span className="appName">Vetap</span>
+      </div>
+      <div className="right">
+        {loggedIn ? (
+          <div className="userPanel">
+            <button className="userButton" onClick={handleSettingsClick}>
+              <img src={`${process.env.PUBLIC_URL}/user.png`} alt="User Icon" className="userIcon" />
+              <span>Ime Korisnika</span>
+            </button>
+            {showSettings && (
+              <div className="settings">
+                {/* Opcije za podešavanje */}
+                <button onClick={handleLogoutClick}>Odjavi se</button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <button className="loginButton" onClick={handleLoginClick}>Prijavi se</button>
+        )}
+      </div>
+    </header>
+  );
+}
+
+export default Header;
+
