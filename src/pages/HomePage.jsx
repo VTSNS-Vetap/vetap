09a96fb8a66d7607f32d5787a7e40cc2470aa47b
Ivan Tetkic <ivan.tetkic@gmail.com>
2024-04-09 22:02:45 +0200

    AG-1 HomePage updat, started with icons on cards

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-
-import { Card, CardContent, CardActionArea, CardMedia, Typography, Grid } from '@mui/material';
+import { Email, Phone, LocationOn, Person } from '@mui/icons-material';
+import { Card, CardContent, CardActionArea, CardMedia, Typography, Grid, Box , Icon} from '@mui/material';
 
 
 
 const HomePage = () => {
 
   return (
-    <>
+    <Box sx={{margin : "auto"}}>
       <Grid  
         rowSpacing={1} 
         columnSpacing={{ xs: 1, sm: 3, md: 5 }}
@@ -15,10 +15,12 @@ const HomePage = () => {
         direction="row"
         justifyContent="center"
         alignItems="center"
+        marginBottom="10px"
+        marginTop="10px<"
         >
        
         <Grid  item xs={6} sm={5} md={4} lg={3}>
-          <Card >
+          <Card>
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -30,7 +32,7 @@ const HomePage = () => {
                 <Typography gutterBottom variant="h5" component="div">
                   Kartoni
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="body2" color="text.secondary" sx={{height: "40px"}}>
                   Praćenje medicinskih kartona, istorije bolesti
                 </Typography>
               </CardContent>
@@ -40,17 +42,17 @@ const HomePage = () => {
         <Grid item xs={6} sm={5} md={4} lg={3}>
           <Card >
             <CardActionArea>
-              <CardMedia
+            <CardMedia
                 component="img"
                 height="140"
                 image="/static/images/cards/usluge.jpg"
-                alt="---usluge---"
+                alt="---Usluge---"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                   Usluge
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="body2" color="text.secondary" sx={{height: "40px"}}>
                   Kreiranje, ažuriranje i pregled usluga koje se pružaju
                 </Typography>
               </CardContent>
@@ -58,8 +60,15 @@ const HomePage = () => {
           </Card>
         </Grid>
         
-        
-          <div></div>
+      </Grid>
+      <Grid  
+        rowSpacing={1} 
+        columnSpacing={{ xs: 1, sm: 3, md: 5 }}
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        >
         <Grid item xs={6} sm={5} md={4} lg={3}>
           <Card >
             <CardActionArea>
@@ -73,7 +82,7 @@ const HomePage = () => {
                 <Typography gutterBottom variant="h5" component="div">
                   Artikli           
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="body2" color="text.secondary" sx={{height: "40px"}}>
                   Praćenje inventara, nabavka i upravljanje zalihama medicinskih i drugih potrepština
                 </Typography>
               </CardContent>
@@ -83,17 +92,14 @@ const HomePage = () => {
         <Grid item xs={6} sm={5} md={4} lg={3}>
           <Card >
             <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                image="/static/images/cards/kontakti.jpg"
-                alt="---kontakti---"
-              />
+            <CardMedia sx={{height: "140px"}}>
+                <Icon sx={{width:"100%", height:"100%"}} ><Phone sx={{fontSize : "5rem", marginTop:"4rem", color: "gray"}}/></Icon>
+              </CardMedia>
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                   Kontakti
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="body2" color="text.secondary" sx={{height: "40px"}}>
                   Centralizovano mesto za čuvanje kontaktnih informacija vaših klijenata, kolega 
                 </Typography>
               </CardContent>
@@ -102,7 +108,7 @@ const HomePage = () => {
         </Grid>
       </Grid>
       
-    </>
+    </Box>
   )
 };
 
